b39b06ae73b613d0172a80eeca781819498cafd8
UkhovArty <uhov.aa@phystech.edu>
2019-11-01 14:27:14 +0300

    Improved serialization of the nested objects.

--- src/ru/sberbank/Example.java
+++ src/ru/sberbank/Example.java
@@ -6,10 +6,13 @@ public class Example<T extends Object> {
     private double rudePi = 3;
     public String[] str = new String[2];
     public SomeClass someClass;
+    public SomeClass[] someClasses = new SomeClass[2];
     public Example (SomeClass someClass) {
         this.str[0] = "Hey, I've just met u";
         this.str[1] = "And this is crazy";
         this.someClass = someClass;
+        this.someClasses[0] = someClass;
+        this.someClasses[1] = someClass;
     }
 
 }
--- src/ru/sberbank/NestedObjSerializer.java
+++ src/ru/sberbank/NestedObjSerializer.java
@@ -1,11 +1,22 @@
 package ru.sberbank;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NestedObjSerializer {
     public void serializeNestedObj(Object obj) throws IllegalAccessException {
         System.out.println("{");
-        System.out.print("\t");
-        Serializer serializer = new Serializer();
-        serializer.objectAsJson(obj);
+        Class clazz = obj.getClass();
+        Map<String, Object> fields = new HashMap();
+        for (Field field: clazz.getDeclaredFields()) {
+            field.setAccessible(true);
+            System.out.print("\t");
+            System.out.print(field.getName());
+            Object object = field.get(obj);
+            Serializer serializer = new Serializer();
+            serializer.objectAsJson(object);
+        }
         System.out.println("},");
     }
 }
