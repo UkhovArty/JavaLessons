9b585e3f470c6c43c5ebe894e3a1288b4890c9e6
UkhovArty <uhov.aa@phystech.edu>
2019-11-01 11:32:24 +0300

    Improved serialization of the nested objects.

--- src/ru/sberbank/ArraySerializer.java
+++ src/ru/sberbank/ArraySerializer.java
@@ -7,6 +7,7 @@ public class ArraySerializer<T extends Object> {
     public void SerializeArray(T[] o) throws IllegalAccessException {
         System.out.println("{");
         for (T t : o) {
+            System.out.print("\t");
             Serializer serializer = new Serializer();
             serializer.objectAsJson(t);
         }
--- src/ru/sberbank/NestedObjSerializer.java
+++ src/ru/sberbank/NestedObjSerializer.java
@@ -2,7 +2,9 @@ package ru.sberbank;
 
 public class NestedObjSerializer {
     public void serializeNestedObj(Object obj) throws IllegalAccessException {
+       // System.out.println("{");
         Serializer serializer = new Serializer();
         serializer.objectAsJson(obj);
+        //System.out.println("}");
     }
 }
